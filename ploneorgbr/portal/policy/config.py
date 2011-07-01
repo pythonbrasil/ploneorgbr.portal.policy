746b2f43a59360881d7136c5dc6382c5ce711c41
Davi Lima <davi@simplesconsultoria.com.br>
2011-07-01 19:24:52 -0300

    Oculta perfis de desinstalacao

--- ploneorgbr/portal/policy/config.py
+++ ploneorgbr/portal/policy/config.py
@@ -9,5 +9,7 @@ __docformat__ = 'plaintext'
 # (name,locked,hidden,install,profile,runProfile)
 
 PRODUCTS=[
+         ('ploneorgbr.portal.policy',0,1,0,'ploneorgbr.portal.policy:uninstall',0),
          ('ploneorgbr.portal.theme',0,1,1,'ploneorgbr.portal.theme:default',1),
+         ('ploneorgbr.portal.theme',0,1,0,'ploneorgbr.portal.theme:uninstall',0),
          ]
